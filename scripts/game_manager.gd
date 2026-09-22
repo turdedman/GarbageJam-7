614e5b604a2c46dbf2f5cbf381d863d70acc707a
turdedman <turdedturder@gmail.com>
2026-09-22 18:54:01 +0200

    scene changer implementerad

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -10,3 +10,8 @@ var coins = 0
 func add_coin():
 	coins += 1
 	pickup_item_label.text = "Coins: " + str(coins)+ "/" + str(totalcoins)
+	if coins >= totalcoins:
+		call_deferred("change_scene")
+	
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/ending_scene.tscn")
