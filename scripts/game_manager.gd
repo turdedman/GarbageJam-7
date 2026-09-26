3c5efdd751a5a10cfad5d1323f8e3b583f54bf04
Naqeeb Rahmani <naqeeb.rahmani789@gmail.com>
2026-09-26 17:05:35 +0200

    now the player can return to the menu after finishing the game

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,12 +6,23 @@ var coins = 0
 @onready var pickup_item_label = $CanvasLayer/PickupItemLabel
 
 
-
+	
+func _process(delta):
+	
+	if $CanvasLayer/GameWinLabel.visible == true:
+		$CanvasLayer/ReturnToMenuLabel.visible = true
+		
+		if Input.is_action_pressed("enter"):
+			get_tree().change_scene_to_file("res://scenes/menu.tscn")
+		
+		
+		
+		
+	
 func add_coin():
 	coins += 1
 	pickup_item_label.text = "Coins: " + str(coins)+ "/" + str(totalcoins)
-	#if coins >= totalcoins:
-	#	call_deferred("change_scene", "res://scenes/ending_scene.tscn")
+	
 	
 func change_scene(scene_path: String):
 	get_tree().change_scene_to_file(scene_path)
